58b74e56c107cbd9d56fae10bbf141489c05fd4c
SamarZiadat <samarziadat@gmail.com>
2023-01-11 14:53:06 +0000

    build: basic menu

--- run.py
+++ run.py
@@ -11,8 +11,32 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('vinyl_collection')
-vinyls = SHEET.worksheet('vinyls')
 
-data = vinyls.get_all_values()
+def menu():
+    """
+    Get menu option input from user
+    """
+    print('Menu')
+    print('1. Add a new vinyl to the collection')
+    print('2. Display full collection')
+    print('3. Edit collection')
+    print('4. Exit')
 
-print(data)
+menu()
+option = int(input('Enter a number from 1-4 to navigate through the menu: '))
+
+while option != 4:
+    if option == 1:
+        print('Option 1 has been called')
+    elif option == 2:
+        print('Option 2 has been called')
+    elif option == 3:
+        print('Option 3 has been called')
+    else:
+        print('Invalid option.')
+        menu()
+
+    print()
+    option = int(input('Enter a number from 1-4 to navigate through the menu: '))
+
+print('You have now exited the programme.')
