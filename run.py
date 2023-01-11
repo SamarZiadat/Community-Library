586695186cd5ed4797e57d40350eed539ed1d630
SamarZiadat <samarziadat@gmail.com>
2023-01-11 16:44:39 +0000

    build: install tabulate library and create function for menu option 2

--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 
+# define the scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# add credentials to account 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+# authorise the sheet 
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# get the instance of the sheet
 SHEET = GSPREAD_CLIENT.open('vinyl_collection')
 
 def menu():
@@ -28,6 +35,14 @@ def option1():
     """
     print('Option 1 has been called using a function')
 
+def option2():
+    """
+    Displays all sheet data from google doc
+    """
+    sheet_instance = SHEET.get_worksheet(0)
+    records_data = sheet_instance.get_all_records()
+    print(tabulate(records_data, tablefmt="rounded_grid"))
+
 menu()
 option = int(input('Enter a number from 1-4 to navigate through the menu: ').strip())
 
@@ -35,7 +50,7 @@ while option != 4:
     if option == 1:
         option1()
     elif option == 2:
-        print('Option 2 has been called')
+        option2()
     elif option == 3:
         print('Option 3 has been called')
     else:
