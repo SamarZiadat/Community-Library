b426467e2e8c16a32e78646e0d0ac752fe7ee0a0
SamarZiadat <samarziadat@gmail.com>
2023-01-16 13:58:42 +0000

    build: improve all docstring

--- run.py
+++ run.py
@@ -30,13 +30,16 @@ SHEET = GSPREAD_CLIENT.open('vinyl_collection')
 
 def pad_to_centre(l, w):
     """
-    Manual centering
+    Manual centering of ASCI art logo
     """
     padding = ' '*(w//2)
     parts = [padding[0: (w-len(p))//2+1]+p for p in l]
     return '\n'.join(parts)
 
 def logo():
+    """
+    Prints ASCI art logo
+    """
     logo = '''\n\n
 ██████╗░██╗░██████╗░░██████╗░
 ██╔══██╗██║██╔════╝░██╔════╝░
@@ -57,7 +60,8 @@ welcome()
 
 def wipe():
     """
-    Wipes the terminal between certain user interections to improve UX.
+    Wipes the terminal between certain user interections to improve UX
+    This code is from a Geeks for Geeks tutorial: https://www.geeksforgeeks.org/clear-screen-python/
     """
     # for windows
     if name == 'nt':
@@ -68,6 +72,16 @@ def wipe():
         _ = system('clear')
 
 def get_row():
+    """
+    Provides a question to the user asking:
+    What is the row number of the vinyl you would like to delete?
+        Params:
+            While loop requests user to input a number from a range valid row numbers
+            If valid while loop breaks
+            Else requests the user tries again
+        Returns:
+            row: used within edit_collection()
+    """
     while True:
         row = input('What is the row number of the vinyl you would like to delete? ').strip()
         if not int(row):
@@ -80,7 +94,14 @@ def get_row():
 
 def edit_collection():
     """
-    Deletes row from external spreadsheet on users command
+    Prints the only worksheet in the external google sheet.
+    Matches index numbers for each row in the tabulated table with that of the index numbers in the external spreadsheet.
+    Provides a question asking user to confirm their row selection
+        Params:
+            While loop requests user to input y or n
+            If y is chosen, while loop breaks and row is deleted. Confirmation message prints.
+            If n is chosen, user is told to start again and loop runs again
+            Else requests the user tries again
     """
     print('\nYour Vinyl Collection:')
     
@@ -110,6 +131,8 @@ def edit_collection():
 def update_vinyl_worksheet(data):
     """
     Inserts new vinyl data from user to external spreadsheet
+    This user data is collated in add_to_collection()
+    Printed message lets user know addition is successful
     """
     vinyl_worksheet = SHEET.worksheet("vinyls")
     vinyl_worksheet.append_row(data)
@@ -118,26 +141,41 @@ def update_vinyl_worksheet(data):
     main()
 
 def get_artist():
-    while True:
-        artist = input('Enter the artist name: ').strip()
-        if re.match(r"[\s\S]", artist):
-            print(f"The artist name is {artist}\n")
-        else:
-            print("Invalid name\n")
-            continue
-        return artist
+    """
+    Provides a question to the user asking:
+    Enter the artist name:
+    Prints artist name back to user
+        Returns:
+        artist: used within add_to_collection
+    """
+    artist = input('Enter the artist name: ').strip()
+    print(f"The artist name is {artist}\n")
+    return artist
 
 def get_album():
-    while True:
-        album = input('Enter the album title: ').strip()
-        if re.match(r"[\s\S]", album):
-            print(f"The album title is {album}\n")
-        else:
-            print("Invalid album title\n")
-            continue
-        return album
+    """
+    Provides a question to the user asking:
+    Enter the album title:
+    Prints album title back to user
+        Returns:
+        album: used within add_to_collection
+    """
+    album = input('Enter the album title: ').strip()
+    print(f"The album title is {album}\n")
+    return album
 
 def get_year():
+    """
+    Provides a question to the user asking:
+    What year was the album released (format YYYY)?
+        Params:
+            While loop validates if numbers are inputted using re
+            If exclusively numbers are not used, while loop breaks and user is asked to try again
+            If exclusively nubers are used, then it is checked if the string. length is 4. 
+            If not valid, user is asked to try again. If valid, year is printed back to user.
+        Returns:
+            year: used within add_to_collection
+    """
     while True:
         year = input('What year was the album released (format YYYY)? ').strip()
         if not re.match('^[0-9]+$', year): 
@@ -152,15 +190,18 @@ def get_year():
 
 def add_to_collection():
     """ 
-    Gets data on new vinyl entry from user
+    Provides steps for the user to follow to add a new vinyl to the management system
+    Global functions are called in a logical order
+    These functions return values that are compiled into a list and printed back to the user
+    While loop is used to ask the user to confirm the details are correct:
+        If user confirms yes, update_vinyl_worksheet() is called and external worksheet is updated
+        If user inputs no, add_to_collection() starts again
+        If user does not enter y or n, error appears and they are asked to try again
     """
     print("About the latest addition to your collection:\n")
     artist = get_artist()
-    main()
     album = get_album()
-    main()
     year = get_year()
-    main()
     
     # List collate the returned values from functions to confirm new vinyl entry
     new_addition = [
@@ -189,7 +230,7 @@ def add_to_collection():
 
 def display_collection():
     """
-    Displays all sheet data from google doc
+    Displays all sheet data from google doc using tabulate
     """
     print('\nYour Vinyl Collection:')
     sheet_instance = SHEET.get_worksheet(0)
@@ -201,42 +242,49 @@ def display_collection():
     main()
 
 def main():
-    def menu():
-        """
-        Print menu options
-        """
+    """
+    Execute main Python function
+    Provides a Main Menu with 4 choices.
+    Params:
+        Requests user to input a number between 1-4
+        If statements execute a functions dependent on input
+        1: add_to_collection()
+        2: display_collection()
+        3: edit_collection()
+        3: exits system using sys.exit()
+        Else requests the user tries again
+    """
+    wipe()
+    print("\n\n\n\n\n")
+    print('Menu\n'.center(80))
+    print('1. Add a new vinyl to the collection'.center(80))
+    print('2. Display full collection'.center(80))
+    print('3. Edit collection'.center(80))
+    print('4. Exit'.center(80))
+
+    option = input(
+    '\n\nEnter a number from 1-4 to navigate through the menu: ').strip()
+
+    if option == "4":
         wipe()
-        print("\n\n\n\n\n")
-        print('Menu\n'.center(80))
-        print('1. Add a new vinyl to the collection'.center(80))
-        print('2. Display full collection'.center(80))
-        print('3. Edit collection'.center(80))
-        print('4. Exit'.center(80))
+        logo()
+        print('You have now exited the programme.'.center(80))
+        print('To restart the programme, press the restart button.'.center(80))
+        sys.exit(0)
 
-        option = input(
-        '\n\nEnter a number from 1-4 to navigate through the menu: ').strip()
-
-        if option == "4":
-            wipe()
-            logo()
-            print('You have now exited the programme.'.center(80))
-            print('To restart the programme, press the restart button.'.center(80))
-            sys.exit(0)
-
-        if option == "1":
-            wipe()
-            add_to_collection()
-        elif option == "2":
-            wipe()
-            display_collection()
-        elif option == "3":
-            wipe()
-            edit_collection()
-        else:
-            print('Invalid option.')
-            time.sleep(2)
-            menu()
-    menu()
+    if option == "1":
+        wipe()
+        add_to_collection()
+    elif option == "2":
+        wipe()
+        display_collection()
+    elif option == "3":
+        wipe()
+        edit_collection()
+    else:
+        print('Invalid option, please enter a number from 1-4.')
+        time.sleep(2)
+    main()
 
 if __name__ == '__main__':
     main()
